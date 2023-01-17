963c433900fddffe7a7a28a8536ce7a09fc662e1
Thales Fossalussa <thalesvitor2001@gmail.com>
2023-01-17 20:26:14 -0300

    Criando componente photo-form

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -2,10 +2,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { PhotoComponent } from './photo/photo.component';
 import { PhotoListComponent } from './photo-list/photo-list.component';
+import { CommonModule } from '@angular/common';
+import { PhotoFormComponent } from './photo-form/photo-form.component';
 
 @NgModule({
-  declarations: [ PhotoComponent, PhotoListComponent ],
-  imports: [ HttpClientModule ]
+  declarations: [ PhotoComponent, PhotoListComponent, PhotoFormComponent ],
+  imports: [
+    HttpClientModule,
+    CommonModule
+  ]
 })
 export class PhotosModule {
 
