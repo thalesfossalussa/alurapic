5ce60af73ea1aa39730a076a4db396cf3c853480
Thales Fossalussa <thalesvitor2001@gmail.com>
2023-01-17 21:22:15 -0300

    importando ErrorsModule na aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { PhotosModule } from './photos/photos.module';
     BrowserModule,
     PhotosModule,
     AppRoutingModule,
+    ErrorsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
