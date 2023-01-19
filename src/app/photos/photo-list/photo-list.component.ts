fc75e9b70eff2211fa30f4d5dbf409e36be2b935
Thales Fossalussa <thalesvitor2001@gmail.com>
2023-01-18 23:07:29 -0300

    Criando componente LoadMore

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { PhotoService } from './../photo/photo.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subject } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
 
 import { Photo } from '../photo/photo';
-import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'app-photo-list',
@@ -16,10 +16,17 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   photos: Photo[] = [];
   filter: string = '';
   debounce: Subject<string> = new Subject<string>();
+  hasMore: boolean = true;
+  currentPage: number = 1;
+  userName: string = '';
 
-  constructor (private activatedRoute: ActivatedRoute) {}
+  constructor (
+    private activatedRoute: ActivatedRoute,
+    private photoService: PhotoService
+  ) {}
 
   ngOnInit(): void {
+    this.userName = this.activatedRoute.snapshot.params.userName;
     this.photos = this.activatedRoute.snapshot.data.photos;
 
     this.debounce.pipe(debounceTime(300)).subscribe(filter => this.filter = filter);
@@ -28,5 +35,12 @@ export class PhotoListComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.debounce.unsubscribe();
+  }
+
+  load() {
+    this.photoService.listFromUserPaginated(this.userName, ++this.currentPage).subscribe(photos => {
+      this.photos = this.photos.concat(photos);
+      if (!photos.length) this.hasMore = false;
+    })
   }
 }
--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -1,5 +1,5 @@
 import { Photo } from './photo';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -12,5 +12,11 @@ export class PhotoService {
 
   listFromUser (userName:string): Observable<Photo[]> {
     return this.http.get<Photo[]>(API + '/' + userName + '/photos');
+  }
+
+  listFromUserPaginated(userName: string, page: number) {
+    const params = new HttpParams().append('page', page.toString());
+
+    return this.http.get<Photo[]>(API + '/' + userName + '/photos', { params });
   }
 }
