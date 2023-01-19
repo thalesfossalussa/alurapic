272cd25a53d8329833c2b526e3dfd0ce65a45bc8
Thales Fossalussa <thalesvitor2001@gmail.com>
2023-01-18 22:55:11 -0300

    Melhorando performance com debounce

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+
 import { Photo } from '../photo/photo';
 import { PhotoService } from '../photo/photo.service';
 
@@ -8,14 +11,22 @@ import { PhotoService } from '../photo/photo.service';
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
 
   photos: Photo[] = [];
   filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
 
   constructor (private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.photos = this.activatedRoute.snapshot.data.photos;
+
+    this.debounce.pipe(debounceTime(300)).subscribe(filter => this.filter = filter);
+
+  }
+
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
   }
 }
